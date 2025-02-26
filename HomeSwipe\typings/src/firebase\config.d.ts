f6a35120f374e263541626349a3a47eba1b2ba49
BRDY-SCAG <scaggari@nmsu.edu>
2025-02-26 16:16:39 -0700

    new-build with vite
    this should work or im gonna kill myself

---
+++ HomeSwipe\typings/src/firebase\config.d.ts
@@ -0,0 +1,14 @@
+
+declare interface firebaseConfigType {
+	static apiKey: string;
+
+	static authDomain: string;
+
+	static projectId: string;
+
+	static storageBucket: string;
+
+	static messagingSenderId: string;
+
+	static appId: string;
+}
